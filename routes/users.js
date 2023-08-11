fbd8d5088f83976d3c77e719ae137a8d518e4274
User 1 <vokimtrivkt@gmail.com>
2023-08-11 14:58:18 +0700

    feat: update vercel config

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,21 @@ const userRoutes = (app, fs) => {
       res.send(JSON.parse(data));
     });
   });
+
+  // GET USER BY ID
+  app.get('/users/:id', (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      const users = JSON.parse(data);
+      const userId = req.params['id'];
+      const user = users[userId];
+
+      res.send(user);
+    });
+  })
 };
 
 module.exports = userRoutes;
